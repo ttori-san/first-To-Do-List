efbfac7b74d7fd4ed0b99db2ffb03b7b15b0c163
Akira Hattori <akirahattori@AkiranoMacBook-Air.local>
2021-04-21 11:24:21 +0900

    [add]jsファイルを追加しました

---
+++ src/js/todo.js
@@ -0,0 +1,31 @@
+'use strict';
+//
+const addTaskTriger = document.getElementsByClassName('js-addTask__triger')[0];
+const addTaskTarget = document.getElementsByClassName('js-addTask__target')[0];
+const addTaskValue = document.getElementsByClassName('js-addTask__value')[0];
+
+const removeTask = removeButton => {
+    const targetTask = removeButton.closest('li');
+    addTaskTarget.removeChild(targetTask);
+};
+
+const addTask = task => {
+    const listItem = document.createElement('li');
+    const removeButton = document.createElement('button');
+
+    removeButton.innerText = '削除';
+    removeButton.addEventListener ('click', () => {
+        removeTask(removeButton);//引数にremoveButtonを入れる意味は？？
+    });
+
+    listItem.innerText = task;
+
+    listItem.append(removeButton);
+    addTaskTarget.appendChild(listItem);//ulの子要素に追加
+};
+
+addTaskTriger.addEventListener('click', event => {
+    const task = addTaskValue.value;
+    addTask(task);
+    addTaskValue.value = '';
+});
